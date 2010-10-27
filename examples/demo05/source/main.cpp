761735392f133cf303bb689d3d8bc59fdee625d7
Andreas Remar <andreas.remar@gmail.com>
2010-10-27 12:26:10 +0200

    * Demo05 scrolls backgrounds

--- examples/demo05/source/main.cpp
+++ examples/demo05/source/main.cpp
@@ -28,26 +28,54 @@ int main(void)
 
   srand(time(0));
 
-  TRY(rage.setupBackground(Rage::MAIN, 0, Rage::BG_MAP_256x256, 16, 16));
-  TRY(rage.setupBackground(Rage::MAIN, 1, Rage::BG_MAP_256x256, 16, 16));
+  TRY(rage.setupBackground(Rage::MAIN, 0, Rage::BG_MAP_512x512, 16, 16));
+  TRY(rage.setupBackground(Rage::MAIN, 1, Rage::BG_MAP_512x512, 16, 16));
+  TRY(rage.setupBackground(Rage::SUB, 0, Rage::BG_MAP_512x512, 16, 16));
+  TRY(rage.setupBackground(Rage::SUB, 1, Rage::BG_MAP_512x512, 16, 16));
 
 #include "blockdef.h"
   TRY(rage.loadTileSet(Rage::MAIN, &blockDef));
+  TRY(rage.loadTileSet(Rage::SUB, &blockDef));
 
 #include "metroiddef.h"
   TRY(rage.loadTileSet(Rage::MAIN, &metroidDef));
+  TRY(rage.loadTileSet(Rage::SUB, &metroidDef));
 
-  for(int y = 0;y < 12;y++)
-    for(int x = 0;x < 16;x++)
+  for(int y = 0;y < 32;y++)
+    for(int x = 0;x < 32;x++)
       {
-	TRY(rage.setTile(Rage::MAIN, 1, x, y, BLOCK_TILESET, rand()%16+1));
+	if(y > 15)
+	  {
+	    TRY(rage.setTile(Rage::MAIN, 0, x, y, BLOCK_TILESET, rand()%16+1));
+	    TRY(rage.setTile(Rage::SUB, 0, x, y, BLOCK_TILESET, rand()%16+1));
+	  }
+	 else
+	   {
+	     TRY(rage.setTile(Rage::MAIN, 0, x, y, METROID_TILESET, rand()%4));
+	     TRY(rage.setTile(Rage::SUB, 0, x, y, METROID_TILESET, rand()%4));
+	   }
 
-	TRY(rage.setTile(Rage::MAIN, 0, x, y, METROID_TILESET, rand()%4));
+	TRY(rage.setTile(Rage::MAIN, 1, x, y, BLOCK_TILESET, rand()%16+1));
+	TRY(rage.setTile(Rage::SUB, 1, x, y, BLOCK_TILESET, rand()%16+1));
       }
+
+  int y = 0;
+  int dy = 1;
 
   while(1)
     {
-      rage.redraw();
+      y += dy;
+
+      if(y < 0 || y > 256)
+       	dy = -dy;
+
+      TRY(rage.setBackgroundScroll(Rage::MAIN, 0, y, y));
+      TRY(rage.setBackgroundScroll(Rage::MAIN, 1, 0, 256 - y));
+
+      TRY(rage.setBackgroundScroll(Rage::SUB, 0, y, y));
+      TRY(rage.setBackgroundScroll(Rage::SUB, 1, 0, 256 - y));
+
+      TRY(rage.redraw());
     }
 
   return 0;
