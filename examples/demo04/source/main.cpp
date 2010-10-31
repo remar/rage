9ba313cc0c87c4e7d4fe0993a3902442dc29f3ef
Andreas Remar <andreas.remar@gmail.com>
2010-10-31 20:57:55 +0100

    * Add error checking to demo04

--- examples/demo04/source/main.cpp
+++ examples/demo04/source/main.cpp
@@ -6,49 +6,62 @@
 
 #include "rage.h"
 #include <time.h>
+#include <stdio.h>
 
 Rage rage;
 
+void displayError(int line)
+{
+  consoleDemoInit();
+
+  printf("ERROR\n--------------------------------\n"
+	 "%d: %s\n", line, rage.getErrorString());
+
+  while(1){}
+}
+
+#define TRY(x) {if(RAGE_FAILED(x)) displayError(__LINE__);}
+
 int main(void)
 {
+  TRY(rage.init(Rage::BG_MAPMEM_SIZE_16K, Rage::BG_MAPMEM_SIZE_16K));
+
   srand(time(0));
 
-  rage.init(Rage::BG_MAPMEM_SIZE_16K, Rage::BG_MAPMEM_SIZE_16K);
-
-  rage.setupBackground(Rage::MAIN, 0, Rage::BG_MAP_256x256, 16, 16);
+  TRY(rage.setupBackground(Rage::MAIN, 0, Rage::BG_MAP_256x256, 16, 16));
 
 #include "metroiddef.h"
 
-  rage.loadTileSet(Rage::MAIN, &metroidDef);
+  TRY(rage.loadTileSet(Rage::MAIN, &metroidDef));
 
-  u16 map[16*12];
-  for(int i = 0;i < 16*12;i++)
+  u16 map[16*16];
+  for(int i = 0;i < 16*16;i++)
     map[i] = i/16==5?0:(i/16==6?rand()%3+1:rand()%4);
 
-  rage.setMap(Rage::MAIN, 0, METROID_TILESET, map);
+  TRY(rage.setMap(Rage::MAIN, 0, METROID_TILESET, map));
 
 #include "cpngooddef.h"
 
-  rage.loadSprite(Rage::MAIN, &cpngoodDef);
+  TRY(rage.loadSprite(Rage::MAIN, &cpngoodDef));
 
   int good = rage.createSpriteInstance(Rage::MAIN, GOOD_SPRITE);
 
-  rage.selectAnimation(Rage::MAIN, good, WALK_RIGHT);
+  TRY(rage.selectAnimation(Rage::MAIN, good, WALK_RIGHT));
 
   int x = -16;
-  rage.moveSpriteAbs(Rage::MAIN, good, x, 5*16);
+  TRY(rage.moveSpriteAbs(Rage::MAIN, good, x, 5*16));
 
   while(1)
     {
-      rage.moveSpriteRel(Rage::MAIN, good, 1, 0);
+      TRY(rage.moveSpriteRel(Rage::MAIN, good, 1, 0));
 
       if(++x > 255)
 	{
 	  x = -16;
-	  rage.moveSpriteAbs(Rage::MAIN, good, x, 5*16);
+	  TRY(rage.moveSpriteAbs(Rage::MAIN, good, x, 5*16));
 	}
 
-      rage.redraw();
+      TRY(rage.redraw());
     }
 
   return 0;
