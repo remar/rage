6a6a5d0b00afdc337133396092a628bc38487298
Andreas Remar <andreas.remar@gmail.com>
2010-11-04 17:41:44 +0100

    * Screenshots of the demos

--- www/screenshots.php
+++ www/screenshots.php
@@ -4,5 +4,21 @@
 
 Screenshots of the examples that are included in the Rage distribution.
 
+<table>
+
+<tr>
+<td class="shot">Demo 1<br/><img class="border" src="gfx/demo01.png"></td>
+<td class="shot">Demo 2<br/><img class="border" src="gfx/demo02.png"></td>
+<td class="shot">Demo 3<br/><img class="border" src="gfx/demo03.png"></td>
+</tr>
+
+<tr>
+<td class="shot">Demo 4<br/><img class="border" src="gfx/demo04.png"></td>
+<td class="shot">Demo 5<br/><img class="border" src="gfx/demo05.png"></td>
+<td class="shot">Demo 6<br/><img class="border" src="gfx/demo06.png"></td>
+</tr>
+
+</table>
+
 </body>
 </html>
